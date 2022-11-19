57b1149eaa2a87d177166c2c745c14160ee63d01
LiamHunting <hunting.liam@gmail.com>
2022-11-18 19:37:45 -0700

    2.8 Homework Updated

--- 2.8Solution/basePage.ts
+++ 2.8Solution/basePage.ts
@@ -1,27 +1,28 @@
-import { Builder, By, Capabilities, until, WebDriver, WebElement } from "selenium-webdriver";
-const chromedriver = require('chromedriver');
+import {Builder, By, Capabilities, until, WebDriver, WebElement} from "selenium-webdriver";
+const chromedriver = require("chromedriver")
 
 interface Options {
     driver?: WebDriver;
-    url?: string;
+    /* if no driver is supplied, we make one or use the constructor to make one */
+    url?: string; 
+    /* oh my gosh samezies*/
 }
 
 export class BasePage {
     driver: WebDriver;
     url: string;
-    
+
     constructor(options?: Options) {
         if (options && options.driver) this.driver = options.driver;
-        else this.driver = new Builder().withCapabilities(Capabilities.chrome()).build();
-        if (options && options.url) this.url = options.url;
+        else this.driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+        if(options && options.url) this.url = options.url
     }
-
     async navigate(url?: string): Promise<void> {
         if (url) return await this.driver.get(url);
-        else if (this.url) return await this.driver.get(this.url);
+        else if (this.url) return await this.driver.get(this.url)
         else
         return Promise.reject(
-            "BasePage.navigate() needs a url defined on the page objects, or one passed in"
+            "BasePage.navigate() needs a url defined on the page objects, or one passed in."
         )
     }
 
@@ -31,22 +32,18 @@ export class BasePage {
         await this.driver.wait(until.elementIsVisible(element))
         return element;
     }
-
     async click(elementBy: By): Promise<void> {
-        return (await this.getElement(elementBy)).click();
+        return(await this.getElement(elementBy)).click();
     }
-
-    async setInput(elementBy: By, keys: any): Promise<void> {
+    async setInput(elementBy:By, keys: any): Promise<void> {
         let input = await this.getElement(elementBy);
         await input.clear();
-        return input.sendKeys();
+        return input.sendKeys(keys)
     }
-
     async getText(elementBy: By): Promise<string> {
-        return (await this.getElement(elementBy)).getText();
+        return (await this.getElement(elementBy)).getText()
     }
-
     async getAttribute(elementBy: By, attribute: string): Promise<string> {
-        return (await this.getElement(elementBy)).getAttribute(attribute);
+        return (await this.getElement(elementBy)).getAttribute(attribute)
     }
 }
