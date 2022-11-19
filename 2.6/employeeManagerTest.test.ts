d41d765c16d14e698ba910735d20eb65a6b4a590
LiamHunting <hunting.liam@gmail.com>
2022-11-18 19:56:52 -0700

    2.6 Homework Updated

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -1,4 +1,4 @@
-import {Builder, By, Capabilities, until, WebDriver, } from "selenium-webdriver";
+import {Builder, By, Capabilities, until, WebDriver} from "selenium-webdriver";
   const chromedriver = require("chromedriver");
 
   const driver: WebDriver = new Builder()
